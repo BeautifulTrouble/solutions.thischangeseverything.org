ea2206fc561f0492f9aa1e3c257fbd3637d98d34
Phillip Smith <ps@phillipadsmith.com>
2016-02-08 15:43:06 -0800

    Switch to pushState for production site.

--- ui/js/app.js
+++ ui/js/app.js
@@ -27,37 +27,3 @@
 
 // Then the app
 //= require backbone-app.js
-
-$(function() {
-    // Initialize a Showdown converter
-    App.showdown = new Showdown.converter();
-    // Initialize the Backbone router.
-    App.router = new App.Router();
-    // TODO change to PushState
-    Backbone.history.start();
-
-    // Check for orientation change, adjust body class
-    function orient() {  
-        if (window.orientation === 0 || window.orientation === 180) {
-            //$("body").addClass("class", "portrait");
-            $("body").addClass("portrait");
-            $("body").removeClass("landscape");
-            orientation = 'portrait';
-            return false;
-        }
-            else if (window.orientation === 90 || window.orientation === -90) {
-            $("body").addClass("landscape");
-            $("body").removeClass("portrait");
-            orientation = 'landscape';
-            return false;
-        }
-    }
-    /* Call orientation function on page load */
-    $(function(){
-            orient();
-    });
-    /* Call orientation function on orientation change */
-    $(window).bind( 'orientationchange', function(e){
-            orient();
-    });
-});
